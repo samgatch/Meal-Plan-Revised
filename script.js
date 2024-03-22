1fb96273d7c30f1f5d679edf881e4ecc215b954f
[samgatch] <[samgatch@icloud.com]>
2024-03-22 16:31:17 -0400

    Added the meals being displayed after submitting them

--- script.js
+++ script.js
@@ -57,49 +57,81 @@
     //Base Variables
     let meals = [];
     let length;
+    let meal;
+    let name;
+    let ingredients = [];
+    let directions = [];
+    let ingredientsLength;
+    let ingredientsArray;
+    let directionsLength;
+    let directionsArray;
+    const submit = document.getElementById('submit');
+    const mealBox = document.getElementById('mealsStorage');
 
-    //Creating new meals
+    //Function to create new meals in js
+    function CreateMeal (name, ingredients, directions) {
+        this.name = name;
+        this.ingredients = ingredients;
+        this.directions = directions;
+    }
 
-        //Variables
-        let meal;
-        let name;
-        let ingredients = [];
-        let directions = [];
-        let ingredientsLength;
-        let ingredientsArray;
-        let directionsLength;
-        let directionsArray;
-        const submit = document.getElementById('submit');
+    //Function to add created meals to the meals array
+    function pushMeal (name, ingredients, directions) {
+        meal = new CreateMeal(name,ingredients,directions);
+        meals.push(meal);
+    }
+    
+    //Adding meals from the add meal box
+    function pullMeal () {
+        name = document.getElementById('name').value;
+        ingredientsArray = document.getElementById('ingList').getElementsByTagName('li');
+        ingredientsLength = ingredientsArray.length;
+        for(let i=0; i<ingredientsLength; i++){
+            ingredients.push(ingredientsArray[i].firstChild.value);
+        };
+        directionsArray = document.getElementById('dirList').getElementsByTagName('li');
+        directionsLength = directionsArray.length;
+        for(let i=0; i<directionsLength; i++){
+            directions.push(directionsArray[i].firstChild.value);
+        };
+        pushMeal(name,ingredients,directions);
+        displayMeals();
+        closeAddMeal();
+    }
 
-        //Function to create new meals in js
-        function CreateMeal (name, ingredients, directions) {
-            this.name = name;
-            this.ingredients = ingredients;
-            this.directions = directions;
+    //Function to display the meals
+    function displayMeals () {
+        length = meals.length;
+        for(let i=0; i<length; i++) {
+            const mealDiv = document.getElementById('mealsStorage');
+            const mealEl = document.createElement('div');
+            mealEl.classList.add('mealEl');
+            const mealTitle = document.createElement('h2');
+            const mealIng = document.createElement('div');
+            const ingList = document.createElement('ul');
+            const mealDir = document.createElement('div');
+            const dirList = document.createElement('ul');
+            let titleData = meals[i].name;
+            let ingData = meals[i].ingredients;
+            let dirData = meals[i].directions;
+            mealTitle.innerHTML = titleData;
+            for(let j=0; j<ingData.length; j++) {
+                const ingItem = document.createElement('li');
+                ingItem.innerHTML = ingData[j];
+                ingList.appendChild(ingItem);
+            }
+            for(let k=0; k<dirData.length; k++) {
+                const dirItem = document.createElement('li');
+                dirItem.innerHTML = dirData[k];
+                dirList.appendChild(dirItem);
+            }
+            mealIng.appendChild(ingList);
+            mealDir.appendChild(dirList);
+            mealEl.appendChild(mealTitle);
+            mealEl.appendChild(mealIng);
+            mealEl.appendChild(mealDir);
+            mealDiv.appendChild(mealEl);
         }
-
-        //Function to add created meals to the meals array
-        function pushMeal (name, ingredients, directions) {
-            meal = new CreateMeal(name,ingredients,directions);
-            meals.push(meal);
-        }
-
-        //Adding meals from the add meal box
-        function pullMeal () {
-            name = document.getElementById('name').value;
-            ingredientsArray = document.getElementById('ingList').getElementsByTagName('li');
-            ingredientsLength = ingredientsArray.length;
-            for(let i=0; i<ingredientsLength; i++){
-                ingredients.push(ingredientsArray[i].firstChild.value);
-            };
-            directionsArray = document.getElementById('dirList').getElementsByTagName('li');
-            directionsLength = directionsArray.length;
-            for(let i=0; i<directionsLength; i++){
-                directions.push(directionsArray[i].firstChild.value);
-            };
-            pushMeal(name,ingredients,directions);
-            console.log(meals);
-        }
-
-        //On click call
-        submit.onclick = pullMeal;
+    }
+    //On click call
+    submit.onclick = pullMeal;
