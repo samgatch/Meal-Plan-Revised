b31e568c37ee642d592d331f1c427e4b5ed8d4a8
[samgatch] <[samgatch@icloud.com]>
2024-03-22 18:38:30 -0400

    removed the add meal opaque overlay due to an issue with sizing replaced with a shadow to show depth

--- script.js
+++ script.js
@@ -4,18 +4,13 @@
     const addMealBtn = document.getElementById('addMealsBtn');
     const closeMealBtn = document.getElementById('close');
     const addMeal = document.getElementById('addMeal');
-    const overlay = document.getElementById('overlay');
 
     //Functions
     function showAddMeal () {
         addMeal.style.display = ('flex');
-        overlay.style.opacity = ('.9');
-        overlay.style.zIndex = ('1');
     }
     function closeAddMeal () {
         addMeal.style.display = ('none');
-        overlay.style.opacity - ('0');
-        overlay.style.zIndex = ('-1');
     }
 
     //On click calls
@@ -102,7 +97,7 @@
         ingredients = [];
         directions = [];
         closeAddMeal();
-        displayMeals()
+        displayMeals();
     }
 
     //Function to display the meals
@@ -143,6 +138,6 @@
             mealStore.appendChild(mealEl);
         }
     }
-    
+
     //On click call
     submit.onclick = pullMeal;
